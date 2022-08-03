ffb2a2e17a8bc4b02646d5e52a2c2a5d1e520d0f
Prince Amit <princeamitlali@gmail.com>
2022-08-03 22:15:21 +0530

    Time: 534 ms (41.20%), Space: 14.8 MB (58.88%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -0,0 +1,18 @@
+class MyCalendar:
+    def __init__(self):
+        self.eventDates = []
+
+    def book(self, start: int, end: int) -> bool:
+        for laststart,lastend in self.eventDates:
+            if laststart < end and start < lastend: # means there is overlapping
+                return False # so we can return False
+        self.eventDates.append((start,end))
+        return True
+            
+        
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
