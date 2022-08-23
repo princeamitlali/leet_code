b17f0d2947580a657aed563e050e1f57cb8c7fa2
Prince Amit <princeamitlali@gmail.com>
2022-08-23 21:27:13 +0530

    Time: 1448 ms (9.65%), Space: 36.3 MB (35.45%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -0,0 +1,17 @@
+from collections import Counter
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        val = Counter(arr)
+        
+        val = sorted(val.items() , key = lambda kv:kv[1],reverse = True)
+        count = 0 
+        print(val)
+        c = len(arr)
+        last = len(val)
+        for key,value in val:
+            c -= value
+            count += 1
+            if c <= len(arr) //2:
+                break
+                
+        return count
