13825ca5ed57245dbfafc46929689ed20dee54dd
Prince Amit <princeamitlali@gmail.com>
2022-08-26 22:22:06 +0530

    Time: 53 ms (65.24%), Space: 14.1 MB (13.37%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -6,11 +6,9 @@ class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         num = n
         l = len(str(num))
-        print(int("".join(sorted(str(num)))))
         dict = {1:[0,3],2:[4,6],3:[7,9],4:[10,13],5:[14,16],6:[17,19],7:[20,23],8:[24,26],9:[27,29],10:[30,33],11:[34,34]}
 
         val = dict[l]
-        print(val)
         for i  in range(val[0],val[1]+1):
             if int("".join(sorted(str(pow(2,i))))) == int("".join(sorted(str(num)))):
                 return True
