25a692bd6c6c0eea1f437e46a7e8411be1ac7e44
Prince Amit <princeamitlali@gmail.com>
2022-09-20 19:23:24 +0530

    Time: 7668 ms (6.24%), Space: 333.1 MB (6.20%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,40 +1,36 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # nums.sort()
+        # res=[]
+        # for i in range(len(nums)):
+        #     l = i+1
+        #     r = len(nums)-1
+        #     while l<r:
+        #         if nums[i] + nums[l] + nums[r] == 0:
+        #             res.append((nums[i], nums[l], nums[r]))
+        #             l+=1
+        #             r-=1
+        #         elif nums[i] + nums[l] + nums[r]>0:
+        #             r-=1
+        #         else:
+        #             l+=1
+        # return set(res)
+        temp = []
+        n = len(nums)
         nums.sort()
-        res=[]
-        for i in range(len(nums)):
-            l = i+1
-            r = len(nums)-1
-            while l<r:
-                if nums[i] + nums[l] + nums[r] == 0:
-                    res.append((nums[i], nums[l], nums[r]))
-                    l+=1
-                    r-=1
-                elif nums[i] + nums[l] + nums[r]>0:
-                    r-=1
+        for i in range(n-2):
+            low = i+ 1
+            high = n-1
+            while high> low:
+                su = nums[i] + nums[low] + nums[high]
+                if su == 0:
+                    temp.append((nums[i] , nums[low] , nums[high]))
+                    # low += 1
+                    # high -= 1
+                if su > 0:
+                    high -= 1
                 else:
-                    l+=1
-        return set(res)
-#         temp = []
-#         n = len(nums)
-#         nums = sorted(nums)
-#         for i in range(n):
-#             low = i+ 1
-#             high = n-1
-#             while high> low:
-                
-#                 t = [nums[i] , nums[low] , nums[high]]
-#                 su = sum(t)
-#                 # print(t)
-#                 if su == 0:
-#                     if t not in temp:
-#                         temp.append(t)
-#                     # low += 1
-#                     # high -= 1
-#                 if su > 0:
-#                     high -= 1
-#                 else:
-#                     low +=1
-#         return temp
+                    low +=1
+        return set(temp)
                 
         
