e64b1c5c8293f02fe194444c603073ec8ac065e1
Prince Amit <princeamitlali@gmail.com>
2022-09-22 00:20:23 +0530

    Time: 82 ms (30.41%), Space: 13.9 MB (60.92%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        number1 = 0
+        number2 = 0
+        for i in num1:
+            number1 = number1 *10 + (ord(i)-48)
+        for i in num2:
+            number2 = number2 *10 + (ord(i)-48)
+        return str(number1 + number2)
+        
