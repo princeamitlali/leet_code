dac49c63dbf952833a907409fb8653156649fb87
Prince Amit <princeamitlali@gmail.com>
2022-09-22 16:36:02 +0530

    Time: 124 ms (5.53%), Space: 13.8 MB (86.20%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -1,25 +1,25 @@
 from collections import Counter
 class Solution:
     def countAndSay(self, n: int) -> str:
-        temp = 1
+        temp = 11
         if n == 1:
             return "1"
-        while n > 1:
+        while n > 2:
             count = 1
             val = str(temp)
+            # temp = 0
+            # if len(val) == 1:
+            #     temp = 10 + int(val)
+            # else:
             temp = 0
-            if len(val) == 1:
-                temp = 10 + int(val)
-            else:
-                temp = 0
-                val = val + "."
-                for i in range(1,len(val)):
-                    if val[i-1] != val[i]:
-                        temp = temp *10 + count
-                        temp = temp *10 + int(val[i-1])
-                        count = 1
-                    else:
-                        count += 1
+            val = val + "."
+            for i in range(1,len(val)):
+                if val[i-1] != val[i]:
+                    temp = temp *10 + count
+                    temp = temp *10 + int(val[i-1])
+                    count = 1
+                else:
+                    count += 1
             n -= 1
         return str(temp)
             
