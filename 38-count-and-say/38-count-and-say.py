7a13ca96c8eca68390697d7a62dd844366cfa9df
Prince Amit <princeamitlali@gmail.com>
2022-09-22 16:33:10 +0530

    Time: 115 ms (9.15%), Space: 14 MB (53.87%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -9,7 +9,6 @@ class Solution:
         while n > 1:
             count = 1
             val = str(temp)
-            print("v",val)
             dic = c(val)
             temp = 0
             if len(val) == 1:
@@ -19,20 +18,11 @@ class Solution:
                 val = val + "."
                 for i in range(1,len(val)):
                     if val[i-1] != val[i]:
-                        # print("c",count,val[i-1])
                         temp = temp *10 + count
                         temp = temp *10 + int(val[i-1])
                         count = 1
                     else:
                         count += 1
-                # if count > 0:
-                #     temp = temp *10 + count -1
-                #     temp = temp *10 + int(val[-1])
-                    
-                    
-            # for i in dic:  
-            #     temp = temp *10 + dic[i]
-            #     temp = temp * 10 + int(i)
             n -= 1
         return str(temp)
             
