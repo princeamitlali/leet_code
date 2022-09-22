5ec161588c8c7b6890dbb342ef8fef9ec9a65f89
Prince Amit <princeamitlali@gmail.com>
2022-09-22 16:37:34 +0530

    Time: 125 ms (5.24%), Space: 13.9 MB (53.87%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -6,20 +6,14 @@ class Solution:
             return "1"
         while n > 2:
             count = 1
-            val = str(temp)
-            # temp = 0
-            # if len(val) == 1:
-            #     temp = 10 + int(val)
-            # else:
+            val = str(temp) + "."
             temp = 0
-            val = val + "."
             for i in range(1,len(val)):
                 if val[i-1] != val[i]:
                     temp = temp *10 + count
                     temp = temp *10 + int(val[i-1])
-                    count = 1
-                else:
-                    count += 1
+                    count = 0
+                count += 1
             n -= 1
         return str(temp)
             
