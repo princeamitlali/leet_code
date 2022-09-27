6de6941faa2431520962699c82464a3d3e087b35
Prince Amit <princeamitlali@gmail.com>
2022-09-27 13:26:57 +0530

    Time: 1354 ms (16.83%), Space: 15.9 MB (83.88%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.py
@@ -0,0 +1,36 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        n=len(dominoes)
+        dominoes=list(dominoes)
+        flag=0
+        for i in range(n-1,-1,-1):
+            if dominoes[i]=="L":
+                ct=1
+                flag=1
+            elif dominoes[i]=="." and flag==1:
+                dominoes[i]=ct
+                ct+=1
+            elif dominoes[i]=="R":
+                flag=0
+            else:
+                dominoes[i]=0
+        flagr=0
+        for i in range(n):
+            if dominoes[i]=="R":
+                ctr=1
+                flagr=1
+            elif str(dominoes[i]).isdigit() and flagr==1 and abs(ctr)<abs(dominoes[i]) or dominoes[i]==".":
+                dominoes[i]="R"
+                ctr+=1
+            elif str(dominoes[i]).isdigit() and flagr==1 and abs(ctr)==abs(dominoes[i]):
+                dominoes[i]="."
+            elif flagr==1 and dominoes[i]==0:
+                dominoes[i]="R"
+            elif dominoes[i]=="L":
+                flagr=0
+            elif dominoes[i]==0:
+                dominoes[i]="."
+            else:
+                dominoes[i]="L"
+        return "".join(dominoes)
+                
