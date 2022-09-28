2d95579c6d98e6b889a6e0832f913214de20ebf5
Prince Amit <princeamitlali@gmail.com>
2022-09-28 14:49:48 +0530

    Time: 64 ms (11.02%), Space: 13.9 MB (73.64%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -1,18 +1,18 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        val = s.split(" ")
+    def wordPattern(self, p: str, s: str) -> bool:
+        v = s.split(" ")
+        if len(set(p)) != len(set(v)):
+            return False
         
-        
-        if len(val) != len(pattern):
+        if len(v) != len(p):
             return False 
-        def check(p,v):
-            dic = {}
-            for i in range(len(p)):
-                if p[i] not in dic.keys():
-                    dic[p[i]] = v[i]
-                else:
-                    if dic[p[i]] != v[i]:
-                           return False 
-            return True
-        return True and check(pattern,val) and check(val,pattern)
+        dic = {}
+        for i in range(len(p)):
+            if p[i] not in dic.keys():
+                dic[p[i]] = v[i]
+            else:
+                if dic[p[i]] != v[i]:
+                       return False 
+        return True
+        # return True and check(pattern,val) and check(val,pattern)
         
