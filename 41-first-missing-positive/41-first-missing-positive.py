ddb0fec8f283661cbc12042619be6e38df939d20
Prince Amit <princeamitlali@gmail.com>
2022-09-29 17:39:57 +0530

    Time: 233 ms (89.46%), Space: 29.5 MB (46.61%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -13,40 +13,6 @@ class Solution(object):
                 if num> 0:
                     return i
         return i
-#         mi = min(nums)
-#         mx = max(nums)
-#         if mx < 1:
-#             return 1
-#         if mx == 1:
-#             return 2
-#         if mi > 1:
-#             return 1
-            
-#         for i in range(mi,mx):
-#             if i not in nums:
-#                 return i
-#         if mi > 1:
-#             return 1
-#         else:
-#             return mx + 1
-        # mi = pow(2,31)
-        # mx = 0
-        # su = 0
-        # for i in nums:
-        #     if i > 0:
-        #         mi = min(mi,i)
-        #         mx = max(mx,i)
-        #         su += i
-        # val = ((mx*(mx+1) /2)- (mi*(mi+-1) /2)) -su
-        # if mx < 1:
-        #     return 1
-        # if mx == 1:
-        #     return 2
-        # print(val)
-        # if mi > 1:
-        #     return 1
-        # if val < 1:
-        #     return mx + 1
-        # return val
+
         
             
