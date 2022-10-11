8018ef3ece360f53f67137742bd23661047a4eac
Prince Amit <princeamitlali@gmail.com>
2022-10-11 11:01:26 +0530

    Time: 774 ms (74.33%), Space: 31.1 MB (5.42%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -3,13 +3,10 @@ class Solution:
         ra = [nums[0]]
         for i in nums:
             ra.append(min(ra[-1],i))
-        print(ra)
         la = [nums[-1]]
         for i in nums[::-1]:
             la.append(max(la[-1],i))
         la = la[::-1]
-        print(la)
-        
         for i in range(len(nums)):
             if ra[i] < nums[i] < la[i]:
                 return True
