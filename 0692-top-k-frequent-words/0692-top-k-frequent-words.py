d3dd197b816ac51822c56a570ea0b8ad796e257a
Prince Amit <princeamitlali@gmail.com>
2022-10-19 09:17:13 +0530

    Time: 111 ms (53.56%), Space: 13.8 MB (99.56%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,8 +2,8 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]: 
         c = Counter
-        words_dict = c(sorted(words))
-        words_dict = sorted(words_dict.items() , key = lambda items:items[1],reverse=True)
+        # words_dict = 
+        words_dict = sorted(c(sorted(words)).items() , key = lambda items:items[1],reverse=True)
         val=[]
         for i in range(k):
             val.append(words_dict[i][0])
