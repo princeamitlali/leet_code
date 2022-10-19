5aabb6241721fcff3f0ded35de7096f4df65d5bc
Prince Amit <princeamitlali@gmail.com>
2022-10-19 13:15:48 +0530

    Time: 30 ms (97.27%), Space: 14.1 MB (59.71%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        res = [s[y-k:y] for y in range(k,len(s)+k,k)]
+        print(res)
+        rev = True
+        s = ""
+        for i in res:
+            if rev :
+                s += i[::-1]
+                rev = False
+            else:
+                s += i
+                rev = True
+        return s
+                
+        
