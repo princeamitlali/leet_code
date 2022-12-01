8ba3125c75a2349756987989c70f59934b53c841
Prince Amit <princeamitlali@gmail.com>
2022-12-01 10:45:57 +0000

    Time: 48 ms (77.72%), Space: 13.9 MB (77.72%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,7 +1,6 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         count1 = 0 
-        count2 = 0
         left = 0
         li = ['a','e','i','o','u','A','E','I','O','U']
         right = len(s)-1
@@ -9,8 +8,8 @@ class Solution:
             if s[right] in li:
                 count1 += 1
             if s[left] in li:
-                count2 += 1
+                count1 -= 1
             right -= 1
             left += 1
-        return count1 == count2
+        return count1 == 0
         
