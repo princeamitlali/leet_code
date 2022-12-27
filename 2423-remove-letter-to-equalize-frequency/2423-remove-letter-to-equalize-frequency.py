c98be0bb3f61ca0c4702c9aca2e4f1105c395f01
Prince Amit <princeamitlali@gmail.com>
2022-12-27 11:17:30 +0530

    Time: 35 ms (84.26%), Space: 13.8 MB (66.79%) - LeetHub

---
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        for i in range(len(word)):
+            if len(set(Counter(word[:i]+word[i+1:]).values()))==1:
+                return True
+        
+        return False
+        
