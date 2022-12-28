a34350c6beb6e75ff7c5bee541596364006e31f5
Prince Amit <princeamitlali@gmail.com>
2022-12-28 11:38:27 +0530

    Time: 31 ms (88.70%), Space: 13.8 MB (56.39%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
@@ -2,7 +2,6 @@ class Solution:
     def squareIsWhite(self, coordinates: str) -> bool:
         v = ord(coordinates[0])-96
         v += int(coordinates[1])
-        print(v)
         return v % 2 == 1
         
         
