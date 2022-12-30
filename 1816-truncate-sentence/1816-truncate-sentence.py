fc74eda4089ab2238ea74d1f8c34433f6d06cd3f
Prince Amit <princeamitlali@gmail.com>
2022-12-30 14:56:19 +0530

    Time: 38 ms (70.02%), Space: 13.8 MB (97.86%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        
+        for i in range(len(s)):
+            if s[i] == " ":
+                k -= 1
+            if k == 0:
+                return s[:i]
+        return s
+        
