6d5355aca2aef74e113e68b8576f5078912a986c
Prince Amit <princeamitlali@gmail.com>
2022-12-31 10:07:02 +0530

    Time: 807 ms (61.21%), Space: 13.9 MB (68.45%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -9,10 +9,7 @@ class Solution:
                 dic[v] = 1
         print(dic)
         m = 0
-        res = 0
         for i in dic:
-            if dic[i] > m:
-                m = dic[i]
-                res = i
+            m = max(m,dic[i])
         return m
         
