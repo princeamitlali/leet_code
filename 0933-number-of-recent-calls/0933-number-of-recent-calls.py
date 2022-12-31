8307b1c9479ff62bf066eabd1ed126807f4bf687
Prince Amit <princeamitlali@gmail.com>
2022-12-31 11:40:34 +0530

    Time: 296 ms (80.43%), Space: 19.5 MB (48.48%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,17 +1,23 @@
+from collections import deque
 class RecentCounter:
 
     def __init__(self):
-        self.element = []
+        # self.element = []
+        self.que = deque()
         
 
     def ping(self, t: int) -> int:
-        self.element.append(t)
-        c = 0
-        i = len(self.element) -1
-        while i >-1 and self.element[i] >= t-3000:
-            c += 1
-            i -= 1
-        return c
+        # self.element.append(t)
+        # c = 0
+        # i = len(self.element) -1
+        # while i >-1 and self.element[i] >= t-3000:
+        #     c += 1
+        #     i -= 1
+        # return c
+        self.que.append(t)
+        while self.que and self.que[0] < t - 3000:
+            self.que.popleft()
+        return len(self.que)
                 
         
 
