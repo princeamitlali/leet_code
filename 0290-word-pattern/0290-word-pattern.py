02359027926bf1fc6d50e798a65bf612f97b06cf
Prince Amit <princeamitlali@gmail.com>
2023-01-01 09:29:29 +0530

    Time: 37 ms (69.66%), Space: 13.9 MB (74.33%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordPattern(self, p: str, s: str) -> bool:
+        v = s.split(" ")
+        if len(set(p)) != len(set(v)):
+            return False
+        
+        if len(v) != len(p):
+            return False 
+        dic = {}
+        for i in range(len(p)):
+            if p[i] not in dic.keys():
+                dic[p[i]] = v[i]
+            else:
+                if dic[p[i]] != v[i]:
+                       return False 
+        return True
+        
