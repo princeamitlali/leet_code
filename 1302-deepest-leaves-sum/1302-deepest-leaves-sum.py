ee094aa8c6a3e4d6775230bd578d2e8cdbf5f53f
Prince Amit <princeamitlali@gmail.com>
2023-01-02 16:34:05 +0530

    Time: 199 ms (95.57%), Space: 17.7 MB (94.92%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -6,26 +6,36 @@
 #         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        arr = [root,None]
-        # res = []
-        temp = []
-        sol = 0
-        if root is None:
-            return []
-        while len(arr) > 1:
-            node = arr.pop(0)
-            if node is None:
-                # res.append(temp)
-                temp = []
-                arr.append(None)
-            else:
-                temp.append(node.val)
-                sol += node.val
-                if node.left:
-                    arr.append(node.left)
-                if node.right:
-                    arr.append(node.right)
-            sol = sum(temp)
-        # res.append(temp)
-        return sol
+        # arr = [root,None]
+        # # res = []
+        # temp = []
+        # sol = 0
+        # if root is None:
+        #     return []
+        # while len(arr) > 1:
+        #     node = arr.pop(0)
+        #     if node is None:
+        #         # res.append(temp)
+        #         temp = []
+        #         arr.append(None)
+        #     else:
+        #         temp.append(node.val)
+        #         sol += node.val
+        #         if node.left:
+        #             arr.append(node.left)
+        #         if node.right:
+        #             arr.append(node.right)
+        #     sol = sum(temp)
+        # # res.append(temp)
+        # return sol
+        
+        curLayer = [root]
+        while curLayer:
+            lastSum, nextLayer = 0, [] # init sum to zero
+            for node in curLayer:
+                lastSum += node.val # rolling sum
+                if node.left: nextLayer.append(node.left)
+                if node.right: nextLayer.append(node.right)
+            curLayer = nextLayer
+        return lastSum
         
