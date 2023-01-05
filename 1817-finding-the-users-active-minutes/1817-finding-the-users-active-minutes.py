fda937efa242de04610dd443e107075b20da0b0e
Prince Amit <princeamitlali@gmail.com>
2023-01-05 13:59:37 +0530

    Time: 2094 ms (50.98%), Space: 25 MB (38.78%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        dic = {}
+        for i in logs:
+            key = str(i[0])
+            val = i[1]
+            if key in dic.keys():
+                if val in dic[key]:
+                    continue
+                else:
+                    v = dic[key]
+                    v.append(val)
+                    dic[key] = v
+            else:
+                dic[key] = [val]
+        res = [0 for i in range(k)]
+        for i in dic:
+            v = len(dic[i])
+            res[v-1] +=1
+        print(res)
+        
+        return res
+        
