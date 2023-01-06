52de9433b206fa8fd22308aa5c2293b5ddb285a6
Prince Amit <princeamitlali@gmail.com>
2023-01-06 16:46:06 +0530

    Time: 79 ms (98.69%), Space: 16.3 MB (7.15%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,9 +1,8 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # v = 
-        res = []
-        for i in list(combinations([i+1 for i in range(n)],k)):
-            res.append(list(i))
-        return res
+        # res = []
+        # :
+        #     res.append(list(i))
+        return [list(i) for i in list(combinations([i+1 for i in range(n)],k))]
         
