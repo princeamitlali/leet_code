08777594768918b9857bc9e0a14dc1fb2a967673
Prince Amit <princeamitlali@gmail.com>
2023-01-08 09:23:16 +0530

    Time: 115 ms (99.49%), Space: 16.6 MB (59.60%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -6,16 +6,16 @@ class Solution:
 
         def dfs(x, y):
             grid[x][y] = -1
-            g = d = h = b = 0
+            up = down = left = right = 0
             if x + 1 <= m and grid[x + 1][y] == 1:
-                d = dfs(x + 1, y)
+                down = dfs(x + 1, y)
             if y + 1 <= n and grid[x][y + 1] == 1:
-                b = dfs(x, y + 1)
+                right = dfs(x, y + 1)
             if x - 1 >= 0 and grid[x - 1][y] == 1:
-                g = dfs(x - 1, y)
+                up = dfs(x - 1, y)
             if y - 1 >= 0 and grid[x][y - 1] == 1:
-                h = dfs(x, y - 1)
-            return 1 + g + d + b + h
+                left = dfs(x, y - 1)
+            return 1 + up + down + right + left
 
         for i in range(m + 1):
             for j in range(n + 1):
