5fc4e46ff7064fbf03840a88c0f3f0d7cbcb9154
Prince Amit <princeamitlali@gmail.com>
2023-01-10 14:49:48 +0530

    Time: 35 ms (79.40%), Space: 13.8 MB (96.75%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
@@ -4,7 +4,10 @@ class Solution:
         r = -1
         n = len(s)
         for i in s:
-            if s.count(i) > 1:
-                r = max(r,n-s[::-1].index(i) -2 - s.index(i)) 
+            if i not in res:
+                res.append(i)
+                if s.count(i) > 1:
+                    r = max(r,n-s[::-1].index(i) -2 - s.index(i)) 
+            
         return r
         
