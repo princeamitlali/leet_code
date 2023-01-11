a93e818299892ef5fca4c6fadccb05905f0cbe63
Prince Amit <princeamitlali@gmail.com>
2023-01-11 13:08:00 +0530

    Time: 1746 ms (94.01%), Space: 48.5 MB (67.31%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,41 +1,29 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-#         m = len(grid)
-#         n = len(grid[0])
-#         trans_grid = [list(i) for i in zip(*grid)]
-        
-#         res=[[0] * n for i in range(m)]
-#         # print(trans_grid)
-#         for i in range(m):
-#             for j in range(n):
-#                 # print(grid[i], trans_grid[i] )
-#                 res[i][j] = grid[i].count(1) + trans_grid[j].count(1) - grid[i].count(0)- trans_grid[j].count(0)
-#             # print(res)
-#         return res
-
         ones_row = []
         ones_col = []
         zero_row = []
         zero_col = []
+        m,n = len(grid),len(grid[0])
         trans_grid = [list(i) for i in zip(*grid)]
         for row in grid:
             one = sum(row)
-            zero = len(grid[0]) - one
+            zero = n - one
 
             ones_row.append(one)
             zero_row.append(zero)
         for row in trans_grid:
             one = sum(row)
-            zero = len(trans_grid[0]) - one
+            zero = m - one
 
             ones_col.append(one)
             zero_col.append(zero)
 
 
-        result = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        result = [[0] * n for i in range(len(grid))]
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
+        for r in range(m):
+            for c in range(n):
                 result[r][c] = ones_row[r] + ones_col[c] - zero_row[r] - zero_col[c]
 
         return result
