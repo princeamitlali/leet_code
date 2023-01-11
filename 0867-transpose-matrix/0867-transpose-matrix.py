59e45d02ba95b00a695ad096e96d21fc6bcacbc2
Prince Amit <princeamitlali@gmail.com>
2023-01-11 10:54:41 +0530

    Time: 74 ms (86.82%), Space: 14.9 MB (16.32%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,5 +1,5 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        matrix =  list(zip(*matrix))
+        matrix =  [list(i) for i in zip(*matrix)]
         return matrix
         
