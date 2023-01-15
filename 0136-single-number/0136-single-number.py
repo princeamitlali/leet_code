1d13d652c65a391fb2b53b4493af39dbb940c7ee
Prince Amit <princeamitlali@gmail.com>
2023-01-15 07:59:38 +0530

    Time: 134 ms (89.02%), Space: 16.6 MB (93.48%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        temp = 0
+        
+        for i in nums:
+            temp = temp ^ i 
+            
+        return temp
