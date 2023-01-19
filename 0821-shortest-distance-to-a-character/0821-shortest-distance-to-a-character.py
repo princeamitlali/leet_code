53f25f4ed5bbab8f70de536e2fc3c86fff2148c1
Prince Amit <princeamitlali@gmail.com>
2023-01-19 16:44:37 +0530

    Time: 42 ms (87.09%), Space: 14 MB (14.95%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -1,13 +1,6 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
         r = len(s)
-        fl = []
-        for i in s:
-            if i == c:
-                r = 0
-            r += 1
-            fl.append(r)
-        r = len(s)
         bl = []
         for i in s[::-1]:
             if i == c:
@@ -15,9 +8,18 @@ class Solution:
             r += 1
             bl.append(r)
         bl = bl[::-1]
+        fl = []
+        r = len(s)
+        for i in range(len(s)):
+            if s[i] == c:
+                r = 0
+            r += 1
+            fl.append(min(bl[i],r)-1)
+        
+        
         res = []
         for i in range(len(s)):
             res.append(min(fl[i],bl[i])-1)
-        return res
+        return fl
             
         
