68ee7e8f3c8cac71251ee467d8c56464f34b2704
Prince Amit <princeamitlali@gmail.com>
2023-03-02 11:44:54 +0530

    Time: 161 ms (9.88%), Space: 14.3 MB (13.86%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        dic = {}
+        for i,val in enumerate(groupSizes):
+            # print(i,val)
+            if val not in dic:
+                dic[val] = [i]
+            else:
+                v = dic[val]
+                v.append(i)
+                print(v)
+                dic[val] = v
+        res = []
+        for i in dic:
+            val = dic[i]
+            for j in range(0,len(val),i):
+                res.append(val[j:j+i])
+        return res
