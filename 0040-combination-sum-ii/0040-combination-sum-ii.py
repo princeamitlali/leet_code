478875126906b4f343a1b2ae8bbd3a56c9690a0f
Prince Amit <princeamitlali@gmail.com>
2023-03-12 16:06:50 +0530

    Time: 39 ms (98.37%), Space: 14.1 MB (16.59%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+        def dfs(i, path, current_target):
+            if(current_target == 0): # target was reached
+                ans.append(path[:])
+            else:
+                for j in range(i,len(nums)):
+                    if(j!=i and nums[j-1] == nums[j]):  # remove the duplicates
+                        continue
+
+                    if(current_target < nums[j]):
+                        break
+
+                    dfs(j+1, path + [nums[j]], current_target - nums[j])
+            
+        ans = []
+        nums.sort()
+        dfs(0,[],target)
+        return ans
+        
