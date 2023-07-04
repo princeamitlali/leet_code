133612ff20920837a999e7f21c1a231e1ac4cac8
Prince Amit <princeamitlali@gmail.com>
2023-07-04 17:21:11 +0530

    Time: 43 ms (81.47%), Space: 16.3 MB (76.38%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,7 +1,6 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         res = ""
-        
         for i in range(min(len(word1),len(word2))):
             res += word1[i]+word2[i]
         
