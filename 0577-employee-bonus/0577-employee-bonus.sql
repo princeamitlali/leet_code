5fe31045b6b73bc6b6722637475e1100a06f76c9
Prince Amit <princeamitlali@gmail.com>
2023-07-10 12:54:10 +0530

    Time: 2320 ms (31.10%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,2 +1,6 @@
 /* Write your T-SQL query statement below */
-select emp.name,bonus from bonus right join employee emp on emp.empid = bonus.empid where  bonus <1000 or bonus is null
+select emp.name,bonus 
+from bonus 
+right join employee emp 
+on emp.empid = bonus.empid 
+where  bonus <1000 or bonus is null
