90a308e26a0d6503c2d4e98f3504318af44db3b8
Prince Amit <princeamitlali@gmail.com>
2025-07-28 12:59:16 +0530

    Time: 36 ms (8.87%), Space: 19.4 MB (38.09%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,18 +4,10 @@ class Solution:
         n = len(height)
         lmax = [height[0] ] * n
         rmax = [height[-1] ] * n
-        # lma
         for i in range(1,n):
             lmax[i] = max(height[i],lmax[i-1])
             rmax[n-i-1] = max(height[n-i-1],rmax[n-i])
-        # print(rmax)
-        # print(lmax)
-        # temp = height[::-1]
-        # rmax_new = [temp[0]]
-        # for i in range(1,n):
-        #     rmax_new.append(max(temp[i],rmax_new[i-1]))
-        # rmax_new = rmax_new[::-1]
-        # print(rmax_new)
+ 
         for i in range(n):
             res += min(rmax[i],lmax[i]) - height[i]
         return res
