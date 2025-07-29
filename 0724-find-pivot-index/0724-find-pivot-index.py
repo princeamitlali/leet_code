ebb0d6266d2962c29dc495040968ba79a851c22e
Prince Amit <princeamitlali@gmail.com>
2025-07-29 19:05:24 +0530

    Time: 5962 ms (6.74%), Space: 18.6 MB (54.58%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -6,7 +6,6 @@ class Solution:
         for i in range(1,len(nums)):
             sum1 = sum(nums[:i-1])
             sum2 = sum(nums[i:])
-            print(sum1,sum2,i)
             if sum1 == sum2:
                 return i - 1
         if sum(nums[:-1]) == 0:
