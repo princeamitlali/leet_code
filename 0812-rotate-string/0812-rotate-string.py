e4f2f1c561a5d7c65983ef0b6665c5816954c945
Prince Amit <princeamitlali@gmail.com>
2025-08-01 18:29:38 +0530

    Time: 0 ms (100%), Space: 18 MB (9.11%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -1,9 +1,7 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        for i in range(len(s)):
-            s_new =  s[i:] + s[:i]
-            # print(s_new)
-            if s_new == goal:
+        for i in range(len(s)): 
+            if s[i:] + s[:i] == goal:
                 return True
 
         return False
