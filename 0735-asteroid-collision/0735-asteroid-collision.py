8a365aabda28703a59d07366879afe27354ec8a0
Prince Amit <princeamitlali@gmail.com>
2025-08-06 15:18:27 +0530

    Time: 11 ms (15.22%), Space: 18.6 MB (96.63%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,24 +1,20 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         st = []
-        # st.append(asteroids[0])
-        i = 0
-        n = len(asteroids)
-        while i < n: 
-            st.append(asteroids[i])
+        for i in asteroids: 
+            st.append(i)
             # print(st)
-            i += 1
             while len(st) > 1:
+               
                 if st[-1] < 0 and st[-2] > 0:
-                    v = st.pop()
-                    a_v = st.pop()
-                    if abs(v) == abs(a_v):
-                        change = 0
-                    if abs(v) > abs(a_v):
-                        change = 0
-                        st.append(v)
-                    if abs(v) < abs(a_v):
-                        st.append(a_v)
+
+                    if abs(st[-1]) == abs(st[-2]):
+                        st = st[:-2]
+                        continue
+                    if abs(st[-1]) > abs(st[-2]):
+                        st.pop(-2)
+                    else:
+                        st.pop(-1)
                 else:
                     break
         return st
