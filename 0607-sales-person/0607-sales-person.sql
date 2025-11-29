bdb0cc4851eb00c7a8553dd95fb0cf1cf74f90b6
Prince Amit <princeamitlali@gmail.com>
2025-11-29 20:57:56 +0530

    Time: 1240 ms (75.65%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,19 +1,16 @@
 # Write your MySQL query statement below
-WITH CTE AS (
-    select
-    o.sales_id
-    from Orders o
-left join
-Company c
-on o.com_id = c.com_id
-where c.name = "RED"
 
-)
 -- select * from cte
 SELECT
 Distinct name
 from SalesPerson
  
-where sales_id not in (select * from cte)
+where sales_id not in (select
+    o.sales_id
+    from Orders o
+left join
+Company c
+on o.com_id = c.com_id
+where c.name = "RED")
 
 
